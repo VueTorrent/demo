74c0bb1b155d0eca946ece5eef1a19492b429f72
Larsluph <larsluph@larsluph.dev>
2025-01-05 08:32:23 +0000

    Deploying to gh-pages from @ VueTorrent/VueTorrent@5fb92294280f76fa620236a4a6b033a2745718b2 🚀

--- assets/MoveTorrentFileDialog-B9YJCWAQ.js
+++
@@ -1,1 +0,0 @@
-import{_ as o}from"./TorrentDetail-wqVgVdDG.js";import"./ColoredChip.vue_vue_type_script_setup_true_lang-CGkwdY__.js";import"./index-Dqva90EY.js";import"./vue-_F-Ug9aV.js";import"./vuetify-9N-iuqjS.js";export{o as default};
---
+++ assets/MoveTorrentFileDialog-D6yC7PED.js
@@ -0,0 +1,1 @@
+import{_ as o}from"./TorrentDetail-z9RX4BNt.js";import"./ColoredChip.vue_vue_type_script_setup_true_lang-DqaK82LC.js";import"./index-DgClTGvw.js";import"./vue-_F-Ug9aV.js";import"./vuetify-9N-iuqjS.js";export{o as default};
