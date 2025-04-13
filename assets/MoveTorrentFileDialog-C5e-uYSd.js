233ab7b9e79db64af27ee818a4b56fb285eec052
WDaan <daan.wijns@accelins.com>
2025-04-13 16:01:04 +0000

    Deploying to gh-pages from @ VueTorrent/VueTorrent@9cbc80a30e3faa5d208a02eee73ca09a1d9ae058 🚀

---
+++ assets/MoveTorrentFileDialog-C5e-uYSd.js
@@ -0,0 +1,1 @@
+import{_ as o}from"./TorrentDetail-WwfPePUS.js";import"./ColoredChip.vue_vue_type_script_setup_true_lang-FRU-pf7g.js";import"./index-BgjWd_2s.js";import"./vue-CNrckKKA.js";import"./vuetify-CQ2RXDiw.js";export{o as default};
--- assets/MoveTorrentFileDialog-DFGcsZV3.js
+++
@@ -1,1 +0,0 @@
-import{_ as o}from"./TorrentDetail-CUZGxPG0.js";import"./ColoredChip.vue_vue_type_script_setup_true_lang-OiMSlYS8.js";import"./index-7KLG_vU_.js";import"./vue-CNrckKKA.js";import"./vuetify-CQ2RXDiw.js";export{o as default};
